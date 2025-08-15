72cf5c3f829f58dd6a870ee29fdf81d822394fed
Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-08-15 17:03:30 -0300

    chore: add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
@@ -10,6 +9,7 @@ export default defineNuxtConfig({
     "@nuxt/scripts",
     "@nuxt/test-utils",
     "@nuxtjs/supabase",
+    "@nuxtjs/tailwindcss",
   ],
 
   supabase: {
